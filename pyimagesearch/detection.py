7bd7b457551cfd4ccfe33ac450a7cb39bd330aff
Becky Wilson <rebecca.wilson2@sky.uk>
2020-06-05 15:32:48 +0100

    Setting up the people detection

--- pyimagesearch/detection.py
+++ pyimagesearch/detection.py
@@ -4,3 +4,15 @@ from .social_distancing_config import MIN_CONF
 
 import numpy as np
 import cv2
+
+
+def detect_people(frame, net, ln, personIdx=0):
+    # frame - from video file
+    # net - pre-initialized and pre-trained YOLO object detection model
+    # ln - YOLO CNN output layer names
+    # personIdx - getting the object at index position 0 from object file (person)
+
+    (H, W) = frame.shape[:2]
+    # grab the dimensions of the frame and  initialize the list of
+    results = []
+    # results
