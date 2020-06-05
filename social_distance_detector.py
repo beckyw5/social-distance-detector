36e5f29896c0a95a1e7cc14385409213b173e59e
Becky Wilson <rebecca.wilson2@sky.uk>
2020-06-05 15:01:29 +0100

    Tidied up spacing

--- social_distance_detector.py
+++ social_distance_detector.py
@@ -8,6 +8,7 @@ NMS_THRESH = 0.3
 
 # boolean indicating if NVIDIA CUDA GPU should be used
 USE_GPU = False
+
 # define the minimum safe distance (in pixels) that two people can be
 # from each other
 MIN_DISTANCE = 50
