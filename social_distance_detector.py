afb80313222600d38e24d2000dd7ecbec67a521a
Becky Wilson <rebecca.wilson2@sky.uk>
2020-06-11 19:24:43 +0100

    Setting up the path for our object methods

--- social_distance_detector.py
+++ social_distance_detector.py
@@ -14,3 +14,7 @@ ap.add_argument("-i", "--input", type=str, default="", help="path to (optional) 
 ap.add_argument("-o", "--output", type=str, default="", help="path to (optional) output video file")
 ap.add_argument("-d", "--display", type=int, default=1, help="whether or not output frame should be displayed")
 args = vars(ap.parse_args())
+
+# load the Object class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([config.MODEL_PATH, "objects.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
