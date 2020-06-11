2e3c8dcb377a5eaf5fb3941910dfdab32a79ff09
Becky Wilson <rebecca.wilson2@sky.uk>
2020-06-11 19:21:14 +0100

    Setting up imports on social_distance_detector.py

---
+++ social_distance_detector.py
@@ -0,0 +1,9 @@
+# import the necessary packages
+from pyimagesearch import social_distancing_config as config
+from pyimagesearch.detection import detect_people
+from scipy.spatial import distance as dist
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
